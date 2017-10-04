92039d5f6b355dbf495f9d3612b37fded18f1164
Aaron Lidman <aaronlidman@gmail.com>
2017-10-04 16:04:49 -0700

    Initial

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const config = require('./config.json');
+const parse = require('./lib/parse.js');
+const request = require('./lib/request.js');
+
+module.exports.handler = function () {
+    request.get(config.base_url + config.replication_dir + 'state.txt')
+        .then(parse.state)
+        .then(request.getStream)
+        .then(parse.change)
+        .catch((err) => console.log(err));
+};
