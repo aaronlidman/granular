3cbf1e9c54c8e41a8c48fd9e3b45b1e9deff1546
Aaron Lidman <aaronlidman@gmail.com>
2017-10-06 21:33:23 -0700

    Put overall metrics

--- index.js
+++ index.js
@@ -3,13 +3,31 @@
 const config = require('./config.json');
 const parse = require('./lib/parse.js');
 const request = require('./lib/request.js');
+const cwput = require('./lib/cwput.js');
 
 module.exports.handler = function () {
-    console.log(process.env);
-
     request.get(config.base_url + config.replication_dir + 'state.txt')
         .then(parse.state)
         .then(request.getStream)
         .then(parse.change)
+        .then((stats) => {
+            cwput.overallMetrics(stats['_overall']);
+        })
+        .then((stats) => {
+            console.log('user,c_nodes,m_nodes,d_nodes,c_ways,m_ways,d_ways,c_relations,m_relations,d_relations');
+            for (const user in stats) {
+                console.log([user,
+                    (stats[user].cnode || 0),
+                    (stats[user].mnode || 0),
+                    (stats[user].dnode || 0),
+                    (stats[user].cway || 0),
+                    (stats[user].mway || 0),
+                    (stats[user].dway || 0),
+                    (stats[user].crelation || 0),
+                    (stats[user].mrelation || 0),
+                    (stats[user].drelation || 0)
+                ].join(','));
+            }
+        })
         .catch((err) => console.log(err));
 };
