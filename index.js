27ce3e659192eb2dbfbc4544f193464978f49048
Aaron Lidman <aaronlidman@gmail.com>
2017-10-06 18:43:28 -0700

    Let's see what's actually in there

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const parse = require('./lib/parse.js');
 const request = require('./lib/request.js');
 
 module.exports.handler = function () {
+    console.log(process.env);
+
     request.get(config.base_url + config.replication_dir + 'state.txt')
         .then(parse.state)
         .then(request.getStream)
