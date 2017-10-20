693567c50a54bfdf3207ffba6db049547faaed8a
Aaron Lidman <aaronlidman@gmail.com>
2017-10-19 22:38:33 -0700

    Write all user stats to S3

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const config = require('./config.json');
 const parse = require('./lib/parse.js');
 const request = require('./lib/request.js');
 const cwput = require('./lib/cwput.js');
+const write = require('./lib/write.js');
 
 module.exports.handler = function () {
     let time;
@@ -17,23 +18,12 @@ module.exports.handler = function () {
         .then(parse.change)
         .then((stats) => {
             time = stats['_minute'];
+            delete stats['_minute'];
+            // carry this properly
+
             return cwput.overallMetrics(stats, time);
         })
-        .then((stats) => {
-            console.log('user,c_nodes,m_nodes,d_nodes,c_ways,m_ways,d_ways,c_relations,m_relations,d_relations');
-            for (const user in stats) {
-                console.log([user,
-                    (stats[user].cnode || 0),
-                    (stats[user].mnode || 0),
-                    (stats[user].dnode || 0),
-                    (stats[user].cway || 0),
-                    (stats[user].mway || 0),
-                    (stats[user].dway || 0),
-                    (stats[user].crelation || 0),
-                    (stats[user].mrelation || 0),
-                    (stats[user].drelation || 0)
-                ].join(','));
-            }
-        })
+        .then(write.overallFile)
+        .then((data) => console.log(data))
         .catch((err) => console.log(err));
 };
