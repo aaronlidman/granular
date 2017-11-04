32166615d1da61593cccf8130fe694339df91c6a
Aaron Lidman <aaronlidman@gmail.com>
2017-11-03 17:45:35 -0700

    Update a few more tests around filenames

--- test/write.test.js
+++ test/write.test.js
@@ -8,7 +8,7 @@ const write = require('../lib/write.js');
 
 test('minutelyStats', (t) => {
     AWS.mock('S3', 'putObject', (params, callback) => {
-        let time = params.Key.split('/').slice(-1)[0].slice(0, 16) + ':00Z';
+        let time = params.Key.split('/').slice(-1)[0].slice(0, 16);
         let testUser = zlib.gunzipSync(params.Body).toString().split('\n')[0];
         t.equal(testUser, time + ',test,22,17,,,,,,,', 'file contents as expected');
 
@@ -39,13 +39,13 @@ test('minutelyStats', (t) => {
 
     // plain old write
     promises.push(write.minutelyStats({
-        stats: {'2017-10-13T15:20:00Z': {'test': {c_node: 22, m_node: 17}}},
+        stats: {'2017-10-13T15:20': {'test': {c_node: 22, m_node: 17}}},
         state: {sequenceNumber: '002669949'}
     }).then(t.ok).catch(t.error));
 
     // catch missing sequence
     promises.push(write.minutelyStats({
-        stats: {'2017-10-13T15:20:00Z': {'test': {c_node: 22, m_node: 17}}},
+        stats: {'2017-10-13T15:20': {'test': {c_node: 22, m_node: 17}}},
     }).catch((err) => {
         t.equal(err, 'missing sequenceNumber', 'successfully errors on missing sequenceNumber');
     }));
@@ -53,8 +53,8 @@ test('minutelyStats', (t) => {
     // write multiple files
     promises.push(write.minutelyStats({
         stats: {
-            '2017-10-13T15:20:00Z': {'test': {c_node: 22, m_node: 17}},
-            '2017-10-13T15:19:00Z': {'test': {c_node: 22, m_node: 17}}
+            '2017-10-13T15:20': {'test': {c_node: 22, m_node: 17}},
+            '2017-10-13T15:19': {'test': {c_node: 22, m_node: 17}}
         },
         state: {sequenceNumber: '002669949'}
     }).then((data) => {
