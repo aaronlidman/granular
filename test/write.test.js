d49822d311fd111aeb0fc99dc45fcc312ad7a9b4
Aaron Lidman <aaronlidman@gmail.com>
2017-11-09 16:09:28 -0800

    Write to dynamo instead

--- test/write.test.js
+++ test/write.test.js
@@ -7,19 +7,18 @@ const zlib = require('zlib');
 const write = require('../lib/write.js');
 
 test('minutelyStats', (t) => {
-    AWS.mock('S3', 'putObject', function (params, callback) {
-        var fileObj = JSON.parse(zlib.gunzipSync(params.Body));
-        t.deepEqual(fileObj, {test: {cnode: 22, mnode: 17}}, 'file contents as expected');
+    AWS.mock('DynamoDB', 'updateItem', function (params, callback) {
+        var fileObj = JSON.parse(zlib.gunzipSync(params.ExpressionAttributeValues[':stats'].B));
+        t.deepEqual(fileObj, {test: {c_node: 22, m_node: 17}}, 'file contents as expected');
 
         if (params.Key === (
-            'stack/environment/raw-stats/2017-10-13T15:20-002669949.json.gz' ||
-            'stack/environment/raw-stats/2017-10-13T15:19-002669949.json.gz')) {
-            t.ok(params.Key, 'files written to the right place');
+            '2017-10-13T15:20-002669949' || '2017-10-13T15:19-002669949')) {
+            t.ok(params.Key, 'keys are as expected');
         } else {
             t.error();
         }
 
-        callback(null, params.Key);
+        callback(null, {});
     });
 
     AWS.mock('CloudWatch', 'putMetricData', function (params, callback) {
@@ -31,20 +30,20 @@ test('minutelyStats', (t) => {
     process.env.AWS_SECRET_ACCESS_KEY = null;
 
     process.env.Bucket = 'bucket';
-    process.env.Environment = 'environment';
+    process.env.Environment = 'staging';
     process.env.OutputPrefix = 'stack/';
 
     let promises = [];
 
     // plain old write
     promises.push(write.minutelyStats({
-        stats: {'2017-10-13T15:20:00Z': {'test': {cnode: 22, mnode: 17}}},
+        stats: {'2017-10-13T15:20:00Z': {'test': {c_node: 22, m_node: 17}}},
         state: {sequenceNumber: '002669949'}
     }).then(t.ok).catch(t.error));
 
     // catch missing sequence
     promises.push(write.minutelyStats({
-        stats: {'2017-10-13T15:20:00Z': {'test': {cnode: 22, mnode: 17}}},
+        stats: {'2017-10-13T15:20:00Z': {'test': {c_node: 22, m_node: 17}}},
     }).catch((err) => {
         t.equal(err, 'missing sequenceNumber', 'successfully errors on missing sequenceNumber');
     }));
@@ -52,21 +51,18 @@ test('minutelyStats', (t) => {
     // write multiple files
     promises.push(write.minutelyStats({
         stats: {
-            '2017-10-13T15:20:00Z': {'test': {cnode: 22, mnode: 17}},
-            '2017-10-13T15:19:00Z': {'test': {cnode: 22, mnode: 17}}
+            '2017-10-13T15:20:00Z': {'test': {c_node: 22, m_node: 17}},
+            '2017-10-13T15:19:00Z': {'test': {c_node: 22, m_node: 17}}
         },
         state: {sequenceNumber: '002669949'}
     }).then((data) => {
         t.deepEqual(data,
-            [
-                'stack/environment/raw-stats/minutes/2017-10-13T15:20-002669949.json.gz',
-                'stack/environment/raw-stats/minutes/2017-10-13T15:19-002669949.json.gz'
-            ],
-            'successfully wrote multiple files');
+            [{}, {}],
+            'successfully wrote 2 files');
     }).catch(t.error));
 
     Promise.all(promises)
-        .then(AWS.restore)
+        .then(() => { AWS.restore(); })
         .then(t.end)
         .catch((error) => {
             t.error(error);
