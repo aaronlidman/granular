be2d7a468a9fbdd9912783d2ed4995cea969d678
Aaron Lidman <aaronlidman@gmail.com>
2017-11-16 22:29:19 -0800

    Aggregator full circle

--- aggregator.js
+++ aggregator.js
@@ -29,7 +29,7 @@ exports.handler = (event) => {
                 ':sequence': {S: '00'}
             }
         }, (err, data) => {
-            if (err) return console.log('querying', err);
+            if (err) return console.log('error querying', err);
 
             const reducedStats = data.Items.reduce((sum, item) => {
                 const stats = JSON.parse(zlib.gunzipSync(Buffer.from(item.stats.B, 'base64')));
@@ -45,36 +45,68 @@ exports.handler = (event) => {
                 delete: 0
             });
 
-            console.log(key, reducedStats);
-
-            const date = new Date(key);
-            const year = date.getUTCFullYear();
-            const monthAndDate = [date.getUTCMonth() + 1, date.getUTCDate()].join('-');
-            const offset = (date.getUTCHours() * 60) + date.getUTCMinutes();
-
-            // insert into the other table
-            dynamo.updateItem({
-                TableName: process.env.DaysTable,
-                Key: {
-                    'year': {N: year.toString()},
-                    'day': {S: monthAndDate}
-                },
-                UpdateExpression: 'SET #CREATE[' + offset + '] = :create, #MODIFY[' + offset + '] = :modify, #DELETE[' + offset + '] = :delete',
-                ExpressionAttributeNames: {
-                    '#CREATE': 'create',
-                    '#MODIFY': 'modify',
-                    '#DELETE': 'delete'
-                },
-                ExpressionAttributeValues: {
-                    ':create': {N: reducedStats.create.toString()},
-                    ':modify': {N: reducedStats.modify.toString()},
-                    ':delete': {N: reducedStats.delete.toString()}
-                },
-            }, (err, data) => {
-                if (err) return console.log('minute insert', err);
-                console.log(data);
-            });
-
+            insertCounts(dynamo, key, reducedStats, false);
         });
     });
 };
+
+function insertCounts(dynamo, minuteKey, counts, retry) {
+    const date = new Date(minuteKey);
+    const year = date.getUTCFullYear();
+    const monthAndDate = [date.getUTCMonth() + 1, date.getUTCDate()].join('-');
+    const offset = (date.getUTCHours() * 60) + date.getUTCMinutes();
+
+    dynamo.updateItem({
+        TableName: process.env.DaysTable,
+        Key: {
+            'year': {N: year.toString()},
+            'day': {S: monthAndDate}
+        },
+        ConditionExpression: 'attribute_exists(#CREATE) AND attribute_exists(#MODIFY) AND attribute_exists(#DELETE)',
+        UpdateExpression: 'SET #CREATE[' + offset + '] = :create, #MODIFY[' + offset + '] = :modify, #DELETE[' + offset + '] = :delete',
+        ExpressionAttributeNames: {
+            '#CREATE': 'create',
+            '#MODIFY': 'modify',
+            '#DELETE': 'delete'
+        },
+        ExpressionAttributeValues: {
+            ':create': {N: counts.create.toString()},
+            ':modify': {N: counts.modify.toString()},
+            ':delete': {N: counts.delete.toString()}
+        }
+    }, (err, data) => {
+        if (err) {
+            if (!retry && err.code === 'ConditionalCheckFailedException') {
+                dynamo.updateItem({
+                    TableName: process.env.DaysTable,
+                    Key: {
+                        'year': {N: year.toString()},
+                        'day': {S: monthAndDate}
+                    },
+                    UpdateExpression: 'SET #CREATE = if_not_exists(#CREATE, :list), #MODIFY = if_not_exists(#MODIFY, :list), #DELETE = if_not_exists(#DELETE, :list)',
+                    ExpressionAttributeNames: {
+                        '#CREATE': 'create',
+                        '#MODIFY': 'modify',
+                        '#DELETE': 'delete'
+                    },
+                    ExpressionAttributeValues: {
+                        ':list': {L: new Array(1440).fill(-1).map((item) => {
+                            return {'N': item.toString()};
+                        })}
+                    }
+                }, (err) => {
+                    if (err) return console.log(err);
+
+                    console.log('prepped ' + minuteKey + ', now going to insert');
+                    return setTimeout(() => {
+                        insertCounts(dynamo, minuteKey, counts, true);
+                    }, 2500);
+                });
+            } else {
+                return console.log('insert error', err);
+            }
+        } else {
+            console.log('inserted', minuteKey, data);
+        }
+    });
+}
