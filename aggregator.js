7767801b9df70a505c9cabcbdd4f02673f433315
Aaron Lidman <aaronlidman@gmail.com>
2017-11-27 18:16:55 -0800

    New and improved aggregator allowing for arbitrary ranges and job types

--- aggregator.js
+++ aggregator.js
@@ -2,160 +2,195 @@
 
 const AWS = require('aws-sdk');
 const zlib = require('zlib');
-const dynamo = new AWS.DynamoDB({
-    region: process.env.AWS_REGION || 'us-west-2'
-});
+const region = process.env.AWS_REGION || 'us-west-2';
+const dynamo = new AWS.DynamoDB({region: region});
+const sqs = new AWS.SQS({region: region});
 
-exports.getCounts = getCounts;
-exports.mergeCounts = mergeCounts;
-exports.allocateLists = allocateLists;
-exports.writeCounts = writeCounts;
+const userCountsLib = require('./lib/userCounts');
+const overallCountsLib = require('./lib/overallCounts');
 
-exports.handler = event => {
-    removeDuplicates(event.Records)
-        .then(getCounts)
-        .then(mergeCounts)
-        .then(writeCounts)
-        .catch(console.log);
+exports.handler = (event, context, callback) => {
+    processNext(event.detail, callback);
 };
 
-function removeDuplicates(records) {
-    let uniqueMinutes = records.reduce((sum, record) => {
-        return sum.add(record.dynamodb.Keys.minute.S);
-    }, new Set());
-
-    return Promise.resolve(uniqueMinutes);
-}
-
-function getCounts(minutes) {
-    let promises = [];
-
-    minutes.forEach((key) => {
-        let query = new Promise((resolve, reject) => {
-            dynamo.query({
-                TableName: process.env.MinutesTable,
-                Select: 'SPECIFIC_ATTRIBUTES',
-                KeyConditionExpression: '#MIN = :minute and begins_with(#SEQ, :sequence)',
-                ProjectionExpression: '#STATS',
-                ExpressionAttributeNames: {
-                    '#MIN': 'minute',
-                    '#SEQ': 'sequence',
-                    '#STATS': 'stats'
-                },
-                ExpressionAttributeValues: {
-                    ':minute': {S: key},
-                    ':sequence': {S: '00'}
-                }
-            }, (err, data) => {
-                if (err) return reject(err);
-                resolve({key: key, counts: data.Items});
-            });
-        });
-
-        promises.push(query);
-    });
-
-    return Promise.all(promises);
-}
-
-function mergeCounts(counts) {
-    // reduce down multiple stats for the same time period into one
-    // 5:15 - created 5
-    // 5:15 - created 2
-    // 5:15 - created 1
-    // => 5:15 - created 8
-
-    counts = counts.map(countObj => {
-        const reducedStats = countObj.counts.reduce((sum, item) => {
-            const stats = JSON.parse(zlib.gunzipSync(Buffer.from(item.stats.B, 'base64')));
-            const overall = stats['_overall'];
-            return {
-                create: sum.create + (overall.create_node || 0) + (overall.create_way || 0) + (overall.create_relation || 0),
-                modify: sum.modify + (overall.modify_node || 0) + (overall.modify_way || 0) + (overall.modify_relation || 0),
-                delete: sum.delete + (overall.delete_node || 0) + (overall.delete_way || 0) + (overall.delete_relation || 0)
-            };
-        }, {
-            create: 0,
-            modify: 0,
-            delete: 0
-        });
-
-        return {key: countObj.key, counts: reducedStats};
-    });
-
-    return counts;
-}
-
-function allocateLists(keyObject) {
-    return new Promise((resolve, reject) => {
-        dynamo.updateItem({
-            TableName: process.env.DaysTable,
-            Key: {
-                'year': {N: keyObject.year.toString()},
-                'day': {S: keyObject.monthAndDate}
-            },
-            UpdateExpression: 'SET #CREATE = if_not_exists(#CREATE, :list), #MODIFY = if_not_exists(#MODIFY, :list), #DELETE = if_not_exists(#DELETE, :list)',
-            ExpressionAttributeNames: {
-                '#CREATE': 'create',
-                '#MODIFY': 'modify',
-                '#DELETE': 'delete'
-            },
-            ExpressionAttributeValues: {
-                ':list': {L: new Array(1440).fill(-1).map((item) => {
-                    return {'N': item.toString()};
-                })}
+function processNext(context, callback) {
+    getMessage(context)
+        .then(getChildren)
+        .then(mergeChildren)
+        .then(packageValues)
+        .then(writeToDynamo)
+        .then(deleteMessage)
+        .then(() => {
+            processNext(context, callback);
+        })
+        .catch((err) => {
+            if (err.no_messages) {
+                // retries or concurrency checks here in the future
+                return callback(null);
+            } else {
+                err.context = context;
+                console.error(err);
+                return callback(err);
             }
-        }, (err) => {
+        });
+}
+
+function getMessage(context) {
+    return new Promise((resolve, reject) => {
+        if (context.source) {
+            if (context.source === 'trigger.perTenMin') {
+                context.queue = process.env.perTenMinQueue;
+            } else {
+                context.queue = process.env.perMinQueue;
+            }
+        } else {
+            return reject(new Error('missing source'));
+        }
+
+        sqs.receiveMessage({
+            QueueUrl: context.queue,
+            VisibilityTimeout: 10,
+            WaitTimeSeconds: 20
+        }, (err, data) => {
             if (err) return reject(err);
-            resolve();
+            if (!data.Messages) return reject({no_messages: true});
+
+            context.job = JSON.parse(data.Messages[0].Body);
+            context.job.ReceiptHandle = data.Messages[0].ReceiptHandle;
+
+            resolve(context);
         });
     });
 }
 
-function writeCounts(counts, retry) {
-    let promises = [];
+function deleteMessage(context) {
+    return new Promise((resolve, reject) => {
+        sqs.deleteMessage({
+            QueueUrl: context.queue,
+            ReceiptHandle: context.job.ReceiptHandle
+        }, (err, data) => {
+            if (err) return reject(err);
+            delete context.job;
+            resolve(context);
+        });
+    });
+}
 
-    counts.forEach((countObj) => {
-        let update = new Promise((resolve, reject) => {
-            const date = new Date(countObj.key);
-            const year = date.getUTCFullYear();
-            const monthAndDate = [date.getUTCMonth() + 1, date.getUTCDate()].join('-');
-            const offset = (date.getUTCHours() * 60) + date.getUTCMinutes();
+function getChildren(context) {
+    // get all the children for the given parent
 
-            dynamo.updateItem({
-                TableName: process.env.DaysTable,
-                Key: {
-                    'year': {N: year.toString()},
-                    'day': {S: monthAndDate}
-                },
-                ConditionExpression: 'attribute_exists(#CREATE) AND attribute_exists(#MODIFY) AND attribute_exists(#DELETE)',
-                UpdateExpression: 'SET #CREATE[' + offset + '] = :create, #MODIFY[' + offset + '] = :modify, #DELETE[' + offset + '] = :delete',
-                ExpressionAttributeNames: {
-                    '#CREATE': 'create',
-                    '#MODIFY': 'modify',
-                    '#DELETE': 'delete'
-                },
-                ExpressionAttributeValues: {
-                    ':create': {N: countObj.counts.create.toString()},
-                    ':modify': {N: countObj.counts.modify.toString()},
-                    ':delete': {N: countObj.counts.delete.toString()}
-                }
-            }, (err, data) => {
-                if (!err) return resolve(data);
+    return new Promise((resolve, reject) => {
+        // all jobs want these things for now, that might change
+        const projExpression = ['#SEQUENCE', '#OVERALLCOUNTS'];
+        const xprsnAttrNames = {
+            '#PARENT': 'parent',
+            '#OVERALLCOUNTS': 'overallCounts',
+            '#SEQUENCE': 'sequence'
+        };
 
-                if (err.code === 'ConditionalCheckFailedException' && !retry) {
-                    allocateLists({year: year, monthAndDate: monthAndDate})
-                        .then(() => { writeCounts([countObj], true); })
-                        .then(resolve)
-                        .catch(reject);
-                } else {
-                    return reject(err);
-                }
-            });
+        if (context.job.jobType === 'minute' ||
+            context.job.jobType === 'hour') {
+            projExpression.push('#USERCOUNTS');
+            xprsnAttrNames['#USERCOUNTS'] = 'userCounts';
+        }
+
+        dynamo.query({
+            TableName: process.env.MainTable,
+            Select: 'SPECIFIC_ATTRIBUTES',
+            KeyConditionExpression: '#PARENT = :parent',
+            ProjectionExpression: projExpression.join(', '),
+            ExpressionAttributeNames: xprsnAttrNames,
+            ExpressionAttributeValues: {
+                ':parent': {S: context.job.key}
+            }
+        }, (err, data) => {
+            if (err) return reject(err);
+            context.job.children = data.Items;
+            resolve(context);
+        });
+    });
+}
+
+function mergeChildren(context) {
+    // merges children of various formats
+
+    if (context.job.jobType === 'minute') {
+        let userCounts = userCountsLib.mergeTotals(context.job.children);
+        userCounts = userCountsLib.collapseTotals(userCounts);
+
+        let overallCounts = overallCountsLib.mergeTotals(context.job.children);
+        overallCounts = overallCountsLib.collapseTotals(overallCounts);
+
+        context.job.data = {
+            userCounts: userCounts,
+            overallCounts: overallCounts
+        };
+    }
+
+    if (context.job.jobType === 'hour') {
+        // put totals into the appropriate position in an array
+            // use the sequence
+        // input: array of individual objects holding totals
+        // output: sparse array of totals
+    }
+
+    delete context.job.children;
+    return Promise.resolve(context);
+}
+
+function packageValues(context) {
+    // pack up data for dynamo
+
+    if (context.job.data.userCounts) {
+        context.job.data.userCounts = {
+            B: zlib.gzipSync(JSON.stringify(context.job.data.userCounts))
+        };
+    }
+
+    if (context.job.data.overallCounts) {
+        context.job.data.overallCounts = {
+            B: zlib.gzipSync(JSON.stringify(context.job.data.overallCounts))
+        };
+    }
+
+    return context;
+}
+
+function writeToDynamo(context) {
+    // dynamically write all data
+
+    return new Promise((resolve, reject) => {
+        const attrs = [];
+        for (const attr in context.job.data) {
+            attrs.push(attr);
+        }
+
+        const attrNames = {};
+        const updateExpression = [];
+        const attrValues = {};
+
+        attrs.forEach(attr => {
+            const subName = '#' + attr.toUpperCase();
+            const subValue = ':' + attr;
+            attrNames[subName] = attr;
+            updateExpression.push(subName + ' = ' + subValue);
+            attrValues[subValue] = context.job.data[attr];
         });
 
-        promises.push(update);
+        // come up with a better way of modifying parent and sequence length
+        // like actually using Date(), negative slices feel sketch
+        dynamo.updateItem({
+            TableName: process.env.MainTable,
+            Key: {
+                parent: {S: context.job.key.slice(0, -3)},
+                sequence: {N: context.job.key.slice(-2)}
+            },
+            UpdateExpression: 'SET ' + updateExpression.join(', '),
+            ExpressionAttributeNames: attrNames,
+            ExpressionAttributeValues: attrValues
+        }, (err, data) => {
+            if (err) return reject(err);
+            resolve(context);
+        });
     });
-
-    return Promise.all(promises);
 }
