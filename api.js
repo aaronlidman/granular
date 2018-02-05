48a2f40fc5ac8d11a0d386c51267ffc27b745080
Aaron Lidman <aaronlidman@gmail.com>
2018-02-04 18:54:36 -0800

    Remove logging

--- api.js
+++ api.js
@@ -4,26 +4,23 @@ const isotrunc = require('./lib/isotrunc');
 const request = require('./lib/request');
 
 exports.handler = (event, context, callback) => {
-    console.log('event', event);
-    console.log('context', context);
-
+    // no paths or methods, just all GETs and querystrings
     const response = {
         statusCode: 200,
         headers: {},
         isBase64Encoded: false
     };
 
-    // no paths or methods, just all GETs and querystrings
-
     let time = (event.queryStringParameters || {}).time;
     if (!time) return callback(null, errorOut(response, 'must specify a time, eg ?time=2018-01-01T01:01'));
 
+    // maximum of minute resolution
     if (time.length > 16) time = time.slice(0, 16);
     const parts = isotrunc(time).parts();
 
     request.countItems(parts.parent, parts.sequence)
         .then(result => {
-            // stream this eventually
+            // ideally api-gateway supports streams in the future
             response.body = JSON.stringify(result);
             return callback(null, response);
         })
